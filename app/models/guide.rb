1d31dbfb84a1ac2965dd533b206cfb11c5df1439
Shogo Hida <shogo.hida@gmail.com>
2020-05-18 20:16:34 +0900

    added has_many and validation for presence

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,3 +1,6 @@
 class Guide < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :location, presence: true
+  validates :biography, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  validates :name, presence: true
 end
