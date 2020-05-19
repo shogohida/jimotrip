343d6eaeb1e1f740d44b0eec1e4a93c0c9d70e04
Julien Ergan <julien.ergan.code@gmail.com>
2020-05-19 16:47:26 +0900

    authentification issues solved with help from Doug

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,18 +1,21 @@
 class GuidesController < ApplicationController
-  
+
    def show
     @guide = Guide.find(params[:id])
+    authorize @guide
   end
-  
+
   def new
     # @user = User.find(params[:user_id])
     @guide = Guide.new
+    authorize @guide
   end
 
   def create
     # @user = User.find(params[:user_id])
     @guide = Guide.new(guide_params)
-    # @guide.user = @user
+    @guide.user = current_user
+    authorize @guide
     if @guide.save
       redirect_to guide_path(@guide)
     else
