a4af2205b5d9eb6840f894e9c737b10e55aca019
Shogo Hida <shogo.hida@gmail.com>
2020-05-19 15:51:35 +0900

    created new and create action for guides controller

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,2 +1,26 @@
 class GuidesController < ApplicationController
+  def new
+    # @user = User.find(params[:user_id])
+    @guide = Guide.new
+  end
+
+  def create
+    # @user = User.find(params[:user_id])
+    @guide = Guide.new(guide_params)
+    # @guide.user = @user
+    if @guide.save
+      redirect_to guide_path(@guide)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def guide_params
+    params.require(:guide).permit(:location, :biography, :price)
+  end
+
+  # :user_id
+
 end
