e8041f9e76e1c7b5d6dd2f7ff669512a297af23e
Julien Ergan <julien.ergan.code@gmail.com>
2020-05-19 12:05:56 +0900

    coded namespacing for guide/booking in the refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -15,40 +15,44 @@ Rails.application.routes.draw do
     resources :bookings, only: :create
   end
 
+  namespace :bookings do
+    resources :guide
+  end
+
   resources :bookings, only: :index
 
 
 
-  # DETAILED ROUTES
+  # # DETAILED ROUTES
 
-  # User Story 1
-  root to: 'pages#home'
+  # # User Story 1
+  # root to: 'pages#home'
 
-  # User Stories 2
-  get '/search', to: 'guides#search', as: :search_guides
+  # # User Stories 2
+  # get '/search', to: 'guides#search', as: :search_guides
 
-  # User Story 3
-  get '/guides/:id', to: 'guides#show', as: :guide
+  # # User Story 3
+  # get '/guides/:id', to: 'guides#show', as: :guide
 
-  # User Story 4 - DON'T USE
+  # # User Story 4 - DON'T USE
 
-  # User Story 5
-  post '/guides/:guide_id/bookings', to: 'bookings#create'
+  # # User Story 5
+  # post '/guides/:guide_id/bookings', to: 'bookings#create'
 
-  # User Story 6
-  get '/bookings', to: 'bookings#index', as: :bookings
+  # # User Story 6
+  # get '/bookings', to: 'bookings#index', as: :bookings
 
-  # User Story 7 - QUESTION - what is the correct syntax for guide/bookings controller?
-  get '/guide/bookings', to: 'guide/bookings#index', as: :guide_bookings
+  # # User Story 7 - QUESTION - what is the correct syntax for guide/bookings controller?
+  # get '/guide/bookings', to: 'guide/bookings#index', as: :guide_bookings
 
-  # User Story 8  - DON'T USE
-  # User Story 9  - DON'T USE
+  # # User Story 8  - DON'T USE
+  # # User Story 9  - DON'T USE
 
-  # User Story 10
-  get 'guides/new', to: 'guides#new', as: :new_guide
+  # # User Story 10
+  # get 'guides/new', to: 'guides#new', as: :new_guide
 
-  # User Story 11
-  post 'guides', to: 'guides#create'
+  # # User Story 11
+  # post 'guides', to: 'guides#create'
 
 end
 
