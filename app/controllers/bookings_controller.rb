a9b92e83f9e49b8786195dd8ada6b02db8e8c71e
Shogo Hida <shogo.hida@gmail.com>
2020-05-22 07:32:01 +0900

    fixed create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,26 +1,29 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = policy_scope(Booking)
+    @bookings = policy_scope(Booking.all)
   end
 
   def create
-    # @guide = Guide.find(params[:_id])
+    @guide = Guide.find(params[:guide_id])
     @booking = Booking.new(booking_params)
     # raise
+    @booking.guide = @guide
     @booking.user = current_user
+    authorize @guide
     authorize @booking
+    # raise
     if @booking.save
       redirect_to bookings_path    # (@booking)?
     # redirect_to booking_path(@booking)
-    else
-      render :index
+    # else
+    #   render :index
     end
   end
 
   private
 
   def booking_params
-   params.require(:booking).permit(:date, :duration, :guide_id)
+   params.require(:booking).permit(:status, :date, :duration, :guide_id)
     # pass status and user_id in permit()?
   end
 end
