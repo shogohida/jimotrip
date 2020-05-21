c7df56e1fe6f6a111a98e3f5607c79240b5ac69d
Shogo Hida <shogo.hida@gmail.com>
2020-05-21 12:59:31 +0900

    added category and price tag

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,4 +1,5 @@
 class Guide < ApplicationRecord
+  CATEGORIES = ['History', 'Nature', 'Culture', 'Art', 'Sports', 'Music', 'Food and Drinks', 'Outdoors', 'Shopping', 'Festivals', 'Relaxation', 'Action and Adventure', 'Touristy', 'Family friendly', 'Great for groups']
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
@@ -7,6 +8,7 @@ class Guide < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: true
   validates :date, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   include PgSearch::Model
   pg_search_scope :search_by_title_location_description_and_price,
     against: [ :title, :location, :description, :price ],
