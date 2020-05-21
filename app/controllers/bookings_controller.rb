8985d1aed016a116d197b89b1a1eff1eeb2f509b
Shogo Hida <shogo.hida@gmail.com>
2020-05-21 14:10:49 +0900

    still missing strong params for create

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,26 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = policy_scope(Booking)
+  end
+
+  def create
+    @guide = Guide.find(params[:guide_id])
+    @booking = Booking.new(booking_params)
+    @booking.guide = @guide
+    authorize @booking
+    if @booking.save
+      redirect_to bookings_path
+      #redirect_to booking_path(@booking)
+    # else
+    #   render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:guide_id)
+    # status set to default as 'Pending'?
+    # pass status in permit()?
+  end
+end
